fdade723e7e6d52d02a0f4c2b2da69c9686adf57
Miikka Koskinen <miikka.koskinen@iki.fi>
2015-08-23 17:29:04 +0300

    Make isovati compile with Rust 1.2.0
    The code is from pre-1.0 times and clearly there has been some
    backwards-incompatible changes since.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
+extern crate bufstream;
 extern crate openssl;
 extern crate toml;
 
-use openssl::ssl::{Tlsv1, SslContext, SslStream};
-use std::io::BufferedStream;
-use std::io::TcpStream;
-use std::io::File;
-use toml::{Table, Value};
+use std::fs::File;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::path::Path;
 
-type IrcStream = BufferedStream<SslStream<TcpStream>>;
+use bufstream::BufStream;
+use openssl::ssl::{SslContext, SslMethod, SslStream};
+use toml::Value;
+
+type IrcStream = BufStream<SslStream<TcpStream>>;
 
 fn slurp_config(path: &str) -> Value {
-    let config_text = File::open(&Path::new(path)).read_to_string().unwrap();
-    let mut parser = toml::Parser::new(config_text.as_slice());
+    let mut config_file = File::open(&Path::new(path)).unwrap();
+    let mut config_text = String::new();
+    let _ = config_file.read_to_string(&mut config_text);
+
+    let mut parser = toml::Parser::new(&config_text[..]);
     match parser.parse() {
-        Some(value) => return Table(value),
+        Some(value) => return Value::Table(value),
         None => {
             println!("Parsing {} failed.", path);
             for error in parser.errors.iter() {
@@ -26,7 +33,7 @@ fn slurp_config(path: &str) -> Value {
     }
 }
 
-#[deriving(Show)]
+#[derive(Debug)]
 enum Message {
     // XXX(miikka) Does it make sense to use String instead of &str? I do not
     // understand Rust well enough to tell.
@@ -34,35 +41,37 @@ enum Message {
     Other(String),
 }
 
+use Message::*;
+
 fn parse_message(msg: String) -> Message {
     // IRC messages are supposed to be separated by CRLF. Drop it.
-    let msg_slice = msg.as_slice().slice_to(msg.len()-2);
+    let msg_slice = &msg[0 .. (msg.len()-2)];
     let parts: Vec<&str> = msg_slice.split(' ').collect();
     match parts[1] {
         "PRIVMSG" => {
-            let mut body = parts.as_slice().slice(3, parts.len()).connect(" ");
+            let mut body = parts[3..].connect(" ");
             let _ = body.remove(0); // drop the initial :
-            return Privmsg(String::from_str(parts[0]),
-                           String::from_str(parts[2]),
-                           body);
+            return Privmsg(String::from(parts[0]),
+                           String::from(parts[2]),
+                           body.clone());
         },
-        _ => return Other(String::from_str(msg_slice)),
+        _ => return Other(String::from(msg_slice)),
     }
 }
 
 fn nick_of(usermask: &String) -> &str {
-    let parts: Vec<&str> = usermask.as_slice().split('!').collect();
+    let parts: Vec<&str> = usermask[..].split('!').collect();
     let nick = parts[0];
     if nick.starts_with(":") {
-        return nick.slice_from(1);
+        return &nick[1..];
     } else {
         return nick;
     }
 }
 
 fn return_chan<'r>(from: &'r String, to: &'r String) -> &'r str {
-    if to.as_slice().starts_with("#") {
-        return to.as_slice();
+    if to[..].starts_with("#") {
+        return &to[..];
     } else {
         return nick_of(from);
     }
@@ -71,10 +80,10 @@ fn return_chan<'r>(from: &'r String, to: &'r String) -> &'r str {
 fn handle_command(irc: &mut IrcStream, msg: Message) {
     match msg {
         Privmsg(from, to, body) => {
-            let parts: Vec<&str> = body.as_slice().split(' ').collect();
+            let parts: Vec<&str> = body[..].split(' ').collect();
             match parts[0] {
                 "^echo" => {
-                    let rest = parts.as_slice().slice_from(1).connect(" ");
+                    let rest = parts[1..].connect(" ");
                     let _ = write!(irc, "PRIVMSG {} :{}\r\n", return_chan(&from, &to), rest);
                     let _ = irc.flush();
                 },
@@ -88,16 +97,16 @@ fn handle_command(irc: &mut IrcStream, msg: Message) {
 fn main() {
     let config = slurp_config("config.toml");
     let server = config.lookup("irc.server").unwrap().as_str().unwrap();
-    let port = config.lookup("irc.port").unwrap().as_integer().unwrap().to_u16().unwrap();
+    let port = config.lookup("irc.port").unwrap().as_integer().unwrap();
     let pw = config.lookup("irc.password").unwrap().as_str().unwrap();
     let nick = config.lookup("irc.nick").unwrap().as_str().unwrap();
     let realname = config.lookup("irc.realname").unwrap().as_str().unwrap();
     let user = config.lookup("irc.username").unwrap().as_str().unwrap();
 
-    let raw_socket = TcpStream::connect(server, port).unwrap();
-    let ssl_ctx = SslContext::new(Tlsv1).unwrap();
+    let raw_socket = TcpStream::connect((server, port as u16)).unwrap();
+    let ssl_ctx = SslContext::new(SslMethod::Tlsv1).unwrap();
     let unbuf_socket = SslStream::new(&ssl_ctx, raw_socket).unwrap();
-    let mut socket = BufferedStream::new(unbuf_socket);
+    let mut socket = BufStream::new(unbuf_socket);
 
     let _ = write!(socket, "USER {} 0 * :{}\r\n", user, realname);
     let _ = write!(socket, "PASS {}\r\n", pw);
@@ -107,9 +116,10 @@ fn main() {
     println!("Receiving now.");
 
     loop {
-        let line = socket.read_line();
-        let msg = parse_message(line.ok().unwrap());
-        println!("{}", msg);
+        let mut line = String::new();
+        let _ = socket.read_line(&mut line);
+        let msg = parse_message(line);
+        println!("{:?}", msg);
         handle_command(&mut socket, msg);
     }
 }
